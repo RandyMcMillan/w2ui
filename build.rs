e998316184834a3dd45cdaa8a2e587d3e4b244a7
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2024-11-27 14:28:30 -0500

    build.rs:src/sass

--- build.rs
+++ build.rs
@@ -8,8 +8,8 @@ use anyhow::Context;
 
 #[derive(Copy, Clone)]
 pub struct Paths<'a> {
-    statics_in_dir: &'a Path,
-    statics_out_dir: &'a Path,
+    sass_in_dir: &'a Path,
+    sass_out_dir: &'a Path,
 }
 
 fn main() {
@@ -22,20 +22,19 @@ fn main() {
 fn run() -> anyhow::Result<()> {
     let manifest_dir =
         PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").context("CARGO_MANIFEST_DIR not set")?);
-    let statics_in_dir = manifest_dir.join("statics");
+    let sass_in_dir = manifest_dir.join("src/sass");
 
     let out_dir = PathBuf::from(std::env::var("OUT_DIR").context("OUT_DIR not set by rustc")?);
-    let statics_out_dir = out_dir.join("statics");
-    let statics_out_dir_css = statics_out_dir.join("css");
-    let statics_out_dir_css_style_css = statics_out_dir_css.join("style.css");
-    println!("{}", statics_out_dir_css_style_css.display());
+    let sass_out_dir = out_dir.join("dist");
+    let sass_out_dir_style_css = sass_out_dir.join("style.css");
+    println!("{}", sass_out_dir_style_css.display());
 
     let paths = Paths {
-        statics_in_dir: &statics_in_dir,
-        statics_out_dir: &statics_out_dir,
+        sass_in_dir: &sass_in_dir,
+        sass_out_dir: &sass_out_dir,
     };
 
-    if let Ok(output) = Command::new("cp").args([statics_out_dir_css_style_css, "./dist".into()]).output(){};
+    if let Ok(output) = Command::new("cp").args([sass_out_dir_style_css, "./dist".into()]).output(){};
     if let Ok(output) = Command::new("git").args(["rev-parse", "HEAD"]).output() {
         if let Ok(git_hash) = String::from_utf8(output.stdout) {
             build_scss(paths).context("Failed to build CSS stylesheets")?;
@@ -48,14 +47,12 @@ fn run() -> anyhow::Result<()> {
 }
 
 fn build_scss(paths: Paths) -> anyhow::Result<()> {
-    let in_dir = paths.statics_in_dir.join("sass");
-    let out_dir = paths.statics_out_dir.join("css");
-    std::fs::create_dir_all(&out_dir).context("Failed to create output directory")?;
+    std::fs::create_dir_all(&paths.sass_out_dir).context("Failed to create output directory")?;
 
-    println!("cargo:rerun-if-changed={}", in_dir.display());
+    println!("cargo:rerun-if-changed={}", paths.sass_in_dir.display());
 
-    let input_file = in_dir.join("style.scss");
-    let output_file = out_dir.join("style.css");
+    let input_file = paths.sass_in_dir.join("style.scss");
+    let output_file = paths.sass_out_dir.join("style.css");
     let format = rsass::output::Format {
         style: rsass::output::Style::Compressed,
         ..rsass::output::Format::default()
